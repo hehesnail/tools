38ed26b4c26e65b5889a6cae5ca068f329f1fdcd
hehesnail <shashen008he@gmail.com>
2019-01-03 18:06:41 +0800

    update

--- tcn.py
+++ tcn.py
@@ -51,7 +51,7 @@ class CausalConv1D(tf.layers.Conv1D):
 """
 Temporal convolutional resblock
 The shortcut connection may need projection to have the same shape ot causal conv ops
-"""    
+"""
 class TemporalBlock(tf.layers.Layer):
     def __init__(self, n_outputs, kernel_size, strides, dilation_rate, dropout=0.2,
                 trainable=True, name=None, dtype=None,
@@ -67,20 +67,20 @@ class TemporalBlock(tf.layers.Layer):
         self.conv1 = CausalConv1D(
             n_outputs, kernel_size, strides=strides,
             dilation_rate=dilation_rate, activation=None,
-            name="conv1")       
+            name="conv1")
         self.conv2 = CausalConv1D(
             n_outputs, kernel_size, strides=strides,
             dilation_rate=dilation_rate, activation=None,
             name="conv2")
         self.down_sample = None
-    
+
     def build(self, input_shape):
         channel_dim = 2
         self.dropout1 = tf.layers.Dropout(self.dropout, [tf.constant(1), tf.constant(1), tf.constant(self.n_outputs)])
         self.dropout2 = tf.layers.Dropout(self.dropout, [tf.constant(1), tf.constant(1), tf.constant(self.n_outputs)])
         if input_shape[channel_dim] != self.n_outputs:
             self.down_sample = tf.layers.Dense(self.n_outputs, activation=None)
-    
+
     def call(self, inputs, training=True):
         x = self.conv1(inputs)
         x = tf.contrib.layers.layer_norm(x)
@@ -95,19 +95,19 @@ class TemporalBlock(tf.layers.Layer):
         if self.down_sample is not None:
             inputs = self.down_sample(inputs)
 
-        return tf.nn.relu(x + inputs)        
+        return tf.nn.relu(x + inputs)
 
 """
 The temporal convnet stacks several temporal conv blocks
-The dilation rate of conv op is 2 ** i in order to make the 
-receptive field conver the whole input sequence. 
+The dilation rate of conv op is 2 ** i in order to make the
+receptive field conver the whole input sequence.
 """
 class TemporalConvNet(tf.layers.Layer):
-    def __init__(self, num_channels, kernel_size=2, dropout=0.2, 
+    def __init__(self, num_channels, kernel_size=2, dropout=0.2,
                 trainable=True, name=None, dtype=None,
                 activity_regularizer=None, **kwargs):
         super(TemporalConvNet, self).__init__(
-            trainable=trainable, dtype=dype, 
+            trainable=trainable, dtype=dtype,
             activity_regularizer=activity_regularizer,
             name=name, **kwargs
         )
